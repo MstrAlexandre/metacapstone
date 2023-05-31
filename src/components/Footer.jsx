5f877e1abc442f5f23c3033996d22c2b4d58bd82
MstrAlexandre <113786195+MstrAlexandre@users.noreply.github.com>
2023-05-31 19:04:59 -0400

    Semantic HTML layout and OGP

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { Nav } from "./Nav"
+
+export const Footer = () => {
+
+    return (
+        <footer>
+            <img></img>
+            <Nav/>
+            <h3>Contact</h3>
+            <p>Address</p>
+            <p>Phone Number</p>
+            <p>Email</p>
+            <h3>Social Media Links</h3>
+            <p>Instagram</p>
+            <p>Twitter</p>
+            <p>Facebook</p>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/logo.svg';
+
+
+
+
+export const Header = () => {
+
+    return (
+        <header>
+            <img src={logo}></img>
+        </header>
+    )
+}
